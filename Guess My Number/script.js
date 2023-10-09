c793f5a6301cc601ce6eb35426c28b91e3fa553e
Kelvin <108274787+ChillBoy16@users.noreply.github.com>
2023-10-09 19:38:21 +0700

    Add files via upload

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-
-// console.log(document.querySelector('.message').
-//     textContent);
+// console.log(document.querySelector('.message').textContent);
 // document.querySelector('.message').textContent = '🎉Correct Number!';
 
 // document.querySelector('.number').textContent = 13;
@@ -11,49 +9,85 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-document.querySelector('.check').addEventListener
-    ('click', function () {
-        const guess = Number(document.querySelector('.guess').value);
-        console.log(guess, typeof guess);
+  //Ketika Tidak ada Input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔ No Number!';
+    displayMessage('⛔ No Number!');
 
-        // Ketika tidak ada input
-        if (!guess) {
-            document.querySelector('.message').textContent = '⛔ No Number!';
+    //Ketika Player Menang
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎉Correct Number!';
+    displayMessage('🎉Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
 
-            // Ketika player menang
-        } else if (guess === secretNumber) {
-            document.querySelector('.message').textContent = '🎉Correct Number!';
-            document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈 Terlalu Tinggi!' : '📉 Terlalu Rendah!';
+      displayMessage(
+        guess > secretNumber ? '📈 Terlalu Tinggi!' : '📉 Terlalu Rendah!'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '💥 Kamu kalah!';
+      displayMessage('💥 Kamu kalah!v');
+      document.querySelector('.number').textContent = 'Oops';
+      document.querySelector('body').style.backgroundColor = '#b34747';
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // Ketika tebakan terlalu tinggi
+  //  else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📈 Terlalu Tinggi!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 Kamu kalah!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
 
-            document.querySelector('body').style.backgroundColor = '#60b347';
-            document.querySelector('.number').style.width = '30rem';
+  // Ketika tebakan terlalu rendah
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = '📉 Terlalu Rendah!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = '💥 Kamu kalah!';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
+});
 
-            // Ketika tebakan terlalu tinggi
-        } else if (guess > secretNumber) {
-            if (score > 1) {
-                document.querySelector('.message').textContent = '📈 Terlalu Tinggi!';
-                score--;
-                document.querySelector('.score').textContent = score;
-            } else {
-                document.querySelector('.message').textContent = '💥 Kamu kalah!';
-                document.querySelector('.score').textContent = 0;
-            }
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // document.querySelector('.message').textContent = 'Mulai menebak...';
+  displayMessage('Mulai menebak...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
 
-            
-            // Ketika tebakan terlalu rendah
-        } else if (guess < secretNumber) {
-
-            if (score > 1) {
-                document.querySelector('.message').textContent = '📉 Terlalu Rendah!';
-                score--;
-                document.querySelector('.score').textContent = score;
-            } else {
-                document.querySelector('.message').textContent = '💥 Kamu kalah!';
-                document.querySelector('.score').textContent = 0;
-            }
-        }
-    });
+  document.querySelector('body').style.backgroundColor = '#131313';
+  document.querySelector('.number').style.width = '15rem';
+});
